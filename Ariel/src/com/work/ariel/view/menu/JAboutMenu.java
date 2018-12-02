590434311b3b9796e1c0cb18ff2e4fff01e41f36
russe <russe@MSI>
2018-12-02 20:43:41 +0900

    Issue #6
    Link to issue: https://github.com/russelonato/ariel/issues/6

--- Ariel/src/com/work/ariel/view/menu/JAboutMenu.java
+++ Ariel/src/com/work/ariel/view/menu/JAboutMenu.java
@@ -1,5 +1,7 @@
 package com.work.ariel.view.menu;
 
+import static javax.swing.JOptionPane.PLAIN_MESSAGE;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,10 +9,14 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
+import com.work.ariel.exception.SystemException;
+import com.work.ariel.util.FileUtil;
+
 public class JAboutMenu extends JMenu implements ActionListener {
 	private static final long serialVersionUID = -6042571009239644612L;
-	
+
 	private JMenuItem mni_about;
+	private JMenuItem mni_viewReadme;
 
 	public JAboutMenu() {
 		super("About");
@@ -19,16 +25,38 @@ public class JAboutMenu extends JMenu implements ActionListener {
 
 	public void initialize() {
 		mni_about = new JMenuItem("About");
+		mni_viewReadme = new JMenuItem("View Readme");
 
 		add(mni_about);
+		add(mni_viewReadme);
 
-		addActionListener(this);
+		mni_about.addActionListener(this);
+		mni_viewReadme.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == mni_about) {
-			JOptionPane.showMessageDialog(this, "A little bit of information", "About", JOptionPane.OK_OPTION);
+			StringBuilder sb = new StringBuilder();
+			
+			sb.append("Version: v2.0.0\n");
+			sb.append("Creators: GABRAMO\n");
+			sb.append("Built using Java, Windows Batch Scripting\n");
+
+			JOptionPane.showMessageDialog(this.getParent().getParent(), sb.toString(), "About", PLAIN_MESSAGE);
+		} else if (e.getSource() == mni_viewReadme) {
+			try {
+				StringBuilder sb = new StringBuilder();
+
+				for (String line : FileUtil.readFile(FileUtil.toFile("resource//readme.txt"))) {
+					sb.append(line + "\n");
+				}
+
+				JOptionPane.showMessageDialog(this.getParent().getParent(), sb.toString(), "About", PLAIN_MESSAGE);
+
+			} catch (SystemException ex) {
+				ex.printStackTrace();
+			}
 		}
 	}
 }
